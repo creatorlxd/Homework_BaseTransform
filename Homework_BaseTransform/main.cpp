d1cafbf71885d88df6f36ac2047e17cb9e0ee943
creatorlxd <1461637345@qq.com>
2020-09-27 22:34:32 +0800

    添加项目文件。

---
+++ Homework_BaseTransform/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "Stack.h"
+
+using namespace std;
+
+int main()
+{
+	int num;
+	int base;
+	cin >> num >> base;
+	Stack<char> s;
+	while (num > 0)
+	{
+		int n = num % base;
+		char c;
+		num /= base;
+		if (n >= 10)
+			c = 'a' + (n - 10);
+		else
+			c = '0' + n;
+		s.Push(c);
+	}
+	while (s.Empty() == false)
+	{
+		cout << s.Top();
+		s.Pop();
+	}
+	cout << endl;
+	return 0;
+}
